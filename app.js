f52f7592fb4966fe22ae30e462f03930130b0612
vivianenguyen <viviane.mgmt@gmail.com>
2021-06-29 18:21:26 -0700

    nav bar
    navigation

---
+++ app.js
@@ -0,0 +1,18 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.mainMenu');
+const openMenu = document.querySelector('.mainMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close() {
+    mainMenu.style.top='-100%';
+}
